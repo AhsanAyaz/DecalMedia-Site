62ebee40ea6e7b9cbfe93baf57886384d639d51a
Keaton Neville <keaton.185@gmail.com>
2018-10-04 19:03:15 +1000

    Change to https

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,13 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  apiUrl:string = "http://beercrawl.crawlmedia.com.au/api/v1";
+  apiUrl:string = "https://beercrawl.crawlmedia.com.au/api/v1";
   constructor(private httpClient : HttpClient) {
 
    }
 
    get_screen(tapCode) { 
-    return this.httpClient.get(this.apiUrl + '/ontap/' + tapCode)
+    return this.httpClient.get(this.apiUrl + '/ontap/22864')
    }
 }
 
